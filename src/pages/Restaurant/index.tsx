47df551dda2210bffd557f4d07f8575232142a29
camilaserra5 <camilaserra5@gmail.com>
2022-09-16 17:53:34 -0300

    remove usless component

--- src/pages/Restaurant/index.tsx
+++ src/pages/Restaurant/index.tsx
@@ -1,7 +1,7 @@
 import { h } from "preact";
 import { useEffect, useState } from "preact/hooks";
+import restaurants from "../../data/restaurants.json";
 import RestaurantCard from "../../components/RestaurantCard";
-import Restaurants from "../../components/Restaurants";
 
 const Home = () => {
   const [test, setTest] = useState(0);
@@ -11,9 +11,13 @@ const Home = () => {
   }, []);
 
   return (
-    <h1>
-      <Restaurants />
-    </h1>
+    <div className="mt-6 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 xl:gap-x-8 gap-y-10 gap-x-6 ">
+      {restaurants.map((restaurant) => (
+        <div key={restaurant.id} className="group relative">
+          <RestaurantCard restaurant={restaurant} />
+        </div>
+      ))}
+    </div>
   );
 };
 
