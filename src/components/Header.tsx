be7cf00cb675cf9b3449bff4902e47f70a2530c3
camilaserra5 <camilaserra5@gmail.com>
2022-09-17 09:27:24 -0300

    improve header responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
+import { h, Ref } from "preact";
 import Logo from "../assets/Logo";
-import PolygonIcon from "../assets/PolygonIcon";
-import LoginButton from "./LoginButton";
-import { h } from "preact";
 import { NavLink } from "react-router-dom";
+import LoginButton from "./LoginButton";
+import PolygonIcon from "../assets/PolygonIcon";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,10 +20,10 @@ function Header() {
                 </NavLink>
               </div>
               <div className="hidden md:block">
-                <div className="ml-20 flex items-baseline space-x-4">
+                <div className="ml-10 flex items-baseline space-x-4">
                   <a
                     href="#"
-                    className=" hover:bg-gray-200  text-black px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-black hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Home
                   </a>
@@ -39,23 +39,32 @@ function Header() {
                     href="#"
                     className="text-black hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium"
                   >
-                    Blog
+                    Restaurantes
                   </a>
 
                   <a
                     href="#"
                     className="text-black hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium"
                   >
-                    Contactanos!
+                    Unite!
                   </a>
                 </div>
+              </div>
+            </div>
+
+            <div class="relative ml-3">
+              <div>
+                <a href="#" className="flex items-center mb-4 sm:mb-0">
+                  <LoginButton />
+                  <PolygonIcon />
+                </a>
               </div>
             </div>
             <div className="-mr-2 flex md:hidden">
               <button
                 onClick={() => setIsOpen(!isOpen)}
                 type="button"
-                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                className="bg-white-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-violet-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               >
@@ -95,14 +104,9 @@ function Header() {
                 )}
               </button>
             </div>
-            <div className="flex-shrink-0">
-              <a href="#" className="flex items-center mb-4 sm:mb-0">
-                <LoginButton />
-                <PolygonIcon />
-              </a>
-            </div>
           </div>
         </div>
+
         <Transition
           show={isOpen}
           enter="transition ease-out duration-100 transform"
@@ -112,43 +116,35 @@ function Header() {
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95"
         >
-          {/* @ts-ignore */}
-          {(ref) => (
+          {(ref: Ref<HTMLDivElement> | undefined) => (
             <div className="md:hidden" id="mobile-menu">
               <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                 <a
                   href="#"
-                  className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-black hover:bg-gray-200  block px-3 py-2 rounded-md text-base font-medium"
                 >
-                  Dashboard
+                  Home
                 </a>
 
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-black hover:bg-gray-200  block px-3 py-2 rounded-md text-base font-medium"
                 >
-                  Team
+                  Quiénes somos
                 </a>
 
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-black hover:bg-gray-200  block px-3 py-2 rounded-md text-base font-medium"
                 >
-                  Projects
+                  Restaurantes
                 </a>
 
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-black hover:bg-gray-200  block px-3 py-2 rounded-md text-base font-medium"
                 >
-                  Calendar
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Reports
+                  Unite!
                 </a>
               </div>
             </div>
