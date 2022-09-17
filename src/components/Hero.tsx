67e55322dc5f992d18798eca4c07492a085084ef
camilaserra5 <camilaserra5@gmail.com>
2022-09-17 14:15:16 -0300

    fix icon error

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,11 +26,19 @@ const Hero = () => {
           Encontrá los mejores lugares y{" "}
           <div class="text-violet-800 inline">
             reservá{" "}
-            <MdPlace
-              style={{
-                display: "inline",
-              }}
-            />
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 24 24"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+              style="display: inline;"
+            >
+              <path fill="none" d="M0 0h24v24H0z"></path>
+              <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 010-5 2.5 2.5 0 010 5z"></path>
+            </svg>
           </div>
         </h1>
       </div>
